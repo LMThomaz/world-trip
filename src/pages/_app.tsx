e5f497c77871cec93dad8c8255f2f70333a5e4b3
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-04-11 16:54:42 -0300

    add banners

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { theme } from "../styles/theme";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,18 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/swiper/swiper-bundle.min.css"
           />
         </Head>
         <body>
